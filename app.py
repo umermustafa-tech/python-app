d30371626b2413c89f7b6f5201965132b21e4de4
Umer Mustafa <umermustafa1070@gmail.com>
2026-05-08 04:29:27 +0500

    Fix greeting message in hello function

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!!!!!!!!!!!!!!!! how are you!!!!!!!!!!!!!!!!!!!!!!!!'
+    return 'Hello, world!!!!!!!!!!!!!!!! how are yougg!!!!!!!!!!!!!!!!!!!!!!!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
