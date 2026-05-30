5abf9d71417f72658230dcf43c499674da9df762
Umer Mustafa <umermustafa1070@gmail.com>
2026-05-31 01:51:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello, world!!!!!!!!!!!!gggggggggggttttttttttuuuuuuuuuuuuuuugggggggggg!!!!fffffffffffffff how tttttttttttre yougg!!!!!!!!!!!!!!!!!!!!!!!!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+print("hello jenkins v1")
